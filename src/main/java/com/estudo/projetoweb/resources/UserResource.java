99ff8ed24428761120e144cf245e0d0e6c4a4944
PauloHenriqque <hhhenriquepaulo@gmail.com>
2025-09-07 14:30:01 -0300

    implement UserService for user management and integrate with UserResource

--- src/main/java/com/estudo/projetoweb/resources/UserResource.java
+++ src/main/java/com/estudo/projetoweb/resources/UserResource.java
@@ -1,19 +1,32 @@
 package com.estudo.projetoweb.resources;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.estudo.projetoweb.entities.User;
+import com.estudo.projetoweb.services.UserService;
 
 @RestController
 @RequestMapping("/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User u = new User(1L, "John Doe", "john@example.com", "1234567890", "password");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
