7e7162463e5b3999fb98463df1203657a77af036
PauloHenriqque <hhhenriquepaulo@gmail.com>
2025-10-30 13:59:53 -0300

    Implement Order management functionality with OrderService and OrderResource; update TestConfig for order initialization

--- src/main/java/com/estudo/projetoweb/config/TestConfig.java
+++ src/main/java/com/estudo/projetoweb/config/TestConfig.java
@@ -1,12 +1,16 @@
 package com.estudo.projetoweb.config;
 
+import java.time.Instant;
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.estudo.projetoweb.entities.Order;
 import com.estudo.projetoweb.entities.User;
 import com.estudo.projetoweb.repositories.UserRepository;
-import java.util.Arrays;
+import com.estudo.projetoweb.repositories.OrderRepository;
 
 @Configuration
 public class TestConfig implements CommandLineRunner {
@@ -14,12 +18,20 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    OrderRepository orderRepository;
+
     @Override
     public void run(String... args) throws Exception {
         
         User u1 = new User(null, "Maria Brown", "maria.brown@example.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex.green@example.com", "977777777", "123456");
-
+        
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+        
         userRepository.saveAll(Arrays.asList(u1, u2));
+        orderRepository.saveAll(Arrays.asList(o1, o2, o3));
     }
 }
--- src/main/java/com/estudo/projetoweb/entities/Order.java
+++ src/main/java/com/estudo/projetoweb/entities/Order.java
@@ -20,6 +20,8 @@ public class Order implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
     @ManyToOne
